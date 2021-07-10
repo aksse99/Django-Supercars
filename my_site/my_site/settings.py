762ffa437954241174a5218ca47a73e409cc842d
Ayush <aksse99@gamil.com>
2021-07-10 23:09:41 +0530

    commit

--- my_site/my_site/settings.py
+++ my_site/my_site/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ['SECRET_KEY']
+SECRET_KEY = 'django-insecure-grw(4c#x9-kn(9v3%6u4&13*wb9vg!*9t^ao&o==hd4@%0^buk'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
