3ceb42b368dfa58a6649a271ae9e92c02d0a3fb4
Ayush <aksse99@gamil.com>
2021-07-10 21:20:50 +0530

    commit

--- my_site/my_site/settings.py
+++ my_site/my_site/settings.py
@@ -22,12 +22,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-grw(4c#x9-kn(9v3%6u4&13*wb9vg!*9t^ao&o==hd4@%0^buk'
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1', 'supercars-web.herokuapp.com']
+ALLOWED_HOSTS = ['supercars-web.herokuapp.com', 'localhost']
 
 
 # Application definition
@@ -123,6 +123,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'static'
 
 
 # Default primary key field type
